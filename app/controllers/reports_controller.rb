a78452a12adb302f065c49e1a76d6d164f6f6d35
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-03 18:31:59 +0900

    日報を作成するための処理を追加するため

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   def index
+    @reports = Book.all
   end
 
   def show; end
@@ -8,11 +9,28 @@ class ReportsController < ApplicationController
   end
 
   def new
+    @report = Report.new
+  end
+
+  def create
+    @report = current_user.reports.build(report_params)
+
+    if @report.save
+      redirect_to report_url(@report), notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def report_params
+    params.require(:report).permit(:title, :content)
   end
 end
