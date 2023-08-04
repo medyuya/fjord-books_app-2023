f74be3697c93047a3bd1876af107d2ddd1b0bc72
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-04 21:18:16 +0900

    コメント削除機能を実装すルタめ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: [:destroy]
+
   def create
     @comment = @commentable.comments.build(comment_params)
     if @comment.save
@@ -9,11 +11,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
