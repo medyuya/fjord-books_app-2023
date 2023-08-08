6d507884c3b8a6e6e46bb548602d50738e618603
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-08 18:13:58 +0900

    ユーザー情報に関して、変種できる項目を追加するため変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[profile address post_code])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[profile address post_code])
   end
 end
