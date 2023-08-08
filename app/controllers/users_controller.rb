7fa6261a6ef2f83d90c0110cf8022c838dcc075c
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-08 18:24:28 +0900

    ユーザー一覧ページにおいて、ページネーションを追加するために変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  ITEMS_PER_PAGE = 3
+
   def index
-    @users = User.all
+    @users = User.page(params[:page]).per(ITEMS_PER_PAGE)
   end
 
   def show
