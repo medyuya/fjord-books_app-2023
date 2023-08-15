c09d1adf824e2e2e9cb812c4ff9040159ac629b8
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-15 19:53:59 +0900

    作成失敗時に、エラーメッセージを追加できるような処理に変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,11 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @commentable.comments.build(comment_params)
+
     if @comment.save
       redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human)
     else
-      redirect_back(fallback_location: root_path)
+      render :new, status: :unprocessable_entity
     end
   end
 
