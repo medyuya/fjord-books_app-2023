288f7a74633492bfaf1dd8f61aa37dbbe388692b
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-15 20:54:16 +0900

    不要なバリデーション処理の削除

--- app/models/mentioned_relationship.rb
+++ app/models/mentioned_relationship.rb
@@ -3,7 +3,5 @@
 class MentionedRelationship < ApplicationRecord
   belongs_to :mentioning, class_name: 'Report'
   belongs_to :mentioned, class_name: 'Report'
-  validates :mentioning_id, presence: true
-  validates :mentioned_id, presence: true
   validates :mentioning_id, uniqueness: { scope: :mentioned_id }
 end
