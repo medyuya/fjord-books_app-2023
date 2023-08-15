ec77eb1ad6e30e69d994b6c5f53792e7a0c1f716
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-15 19:08:12 +0900

    必須の項目はnullで保存できないようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -60,8 +60,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_085814) do
   end
 
   create_table "reports", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
