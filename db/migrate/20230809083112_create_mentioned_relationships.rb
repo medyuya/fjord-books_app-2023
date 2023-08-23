410ff7a8eee63f8e78d5650dfeefb81413acbbd5
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-23 18:52:01 +0900

    DBの関連付けにおいて、DBの制約を追加するため

--- db/migrate/20230809083112_create_mentioned_relationships.rb
+++ db/migrate/20230809083112_create_mentioned_relationships.rb
@@ -1,8 +1,8 @@
 class CreateMentionedRelationships < ActiveRecord::Migration[7.0]
   def change
     create_table :mentioned_relationships do |t|
-      t.integer :mentioning_id
-      t.integer :mentioned_id
+      t.integer :mentioning_id, null: false, foreign_key: true
+      t.integer :mentioned_id, null: false, foreign_key: true
 
       t.timestamps
     end
