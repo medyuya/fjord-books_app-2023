a2056a9f35141b2ddc80f51a8ecddfd7f970e1ec
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-30 21:22:54 +0900

    日報モデルのインスタンスメソッドに対するテストの作成

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,19 +1,11 @@
 FactoryBot.define do
   factory :user do
     email { 'ken@example.com' }
-    password { 'password' }
+    password { 'ken_password' }
+  end
+
+  factory :other_user, class: User do
+    email { 'taro@example.com' }
+    password { 'taro_password' }
   end
 end
-
-
-# t.string "email", default: "", null: false
-# t.string "encrypted_password", default: "", null: false
-# t.string "reset_password_token"
-# t.datetime "reset_password_sent_at"
-# t.datetime "remember_created_at"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# t.string "name"
-# t.string "postal_code"
-# t.string "address"
-# t.text "self_introduction"
--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = FactoryBot.create(:user)
+  end
+
+  test "editable? returns true" do
+    report = FactoryBot.create(:report, user_id: @user.id)
+    assert report.editable?(@user)
+  end
+
+  test "editable? returns false" do
+    other_user = FactoryBot.create(:other_user)
+    report = FactoryBot.create(:report, user_id: other_user.id)
+    assert_not report.editable?(@user)
+  end
+
+  test "created_on returns true" do
+    report = FactoryBot.create(:report, user_id: @user.id, created_at: 'Wed, 30 Aug 2023 21:20:50 JST +09:00')
+    assert_equal 'Wed, 30 Aug 2023', report.created_on
+  end
 end
