512568fe80247c908d31f157c3eb9e15a9b23e5c
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-30 20:59:49 +0900

    name_or_emailメソッドのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name_or_email returns the name" do
+    user = FactoryBot.create(:user, name: 'ken')
+    assert_equal 'ken', user.name_or_email
+  end
+
+  test "name_or_email returns the email" do
+    user = FactoryBot.create(:user)
+    assert_equal 'ken@example.com', user.name_or_email
+  end
 end
