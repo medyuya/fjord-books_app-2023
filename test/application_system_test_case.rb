935f8ef845ec76641fcbf6d0fc34f0de294cafe7
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2023-08-31 17:58:38 +0900

    ログインする処理を共通化するため

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+ require_relative './support/system_test_methods/login_support.rb'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :safari, screen_size: [1400, 1400]
+  include LoginSupport
 end
---
+++ test/support/system_test_methods/login_support.rb
@@ -0,0 +1,10 @@
+module LoginSupport
+  def login_as(user_email, user_password)
+    visit new_user_session_url
+    fill_in 'user[email]', with: user_email
+    fill_in 'user[password]', with: user_password
+    click_on 'ログイン'
+    assert_current_path '/books'
+    assert_selector 'p#notice', text: 'ログインしました。'
+  end
+end
