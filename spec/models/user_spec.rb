41b6522e916d3a85f857abba57e352ab02c1c129
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-04-25 16:46:11 +0900

    name_or_emailメソッドのテストを追加するため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name_or_email' do
+    context 'when name is present' do
+      let!(:user_with_name) { create(:user, name: 'ケン') }
+
+      it 'returns the name' do
+        expect(user_with_name.name_or_email).to eq 'ケン'
+      end
+    end
+
+    context 'when name is not present' do
+      let!(:user_with_email) { create(:user, email: 'ken@example.com') }
+
+      it 'returns the email' do
+        expect(user_with_email.name_or_email).to eq 'ken@example.com'
+      end
+    end
+  end
 end
