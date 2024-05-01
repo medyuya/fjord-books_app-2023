24f7c976c1a489886a181cdfc1b95dcc8c1047ed
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-05-01 13:20:21 +0900

    ログイン操作を共通化するため

---
+++ spec/support/system_spec_methods/login_support.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module LoginSupport
+  def login_as(user)
+    visit new_user_session_path
+    fill_in 'Eメール', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_on 'ログイン'
+
+    # expect(current_path).to eq books_path
+    expect(page).to have_text 'ログインしました。'
+  end
+end
