fcfb4a4086b2abaf2f2624c2b57071d6e654e253
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-05-01 16:02:14 +0900

    命名をより簡潔にするため

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe '#name_or_email' do
     context 'when name is present' do
-      let!(:user_with_name) { create(:user, name: 'ケン') }
+      let!(:user) { create(:user, name: 'ケン') }
 
       it 'returns the name' do
-        expect(user_with_name.name_or_email).to eq 'ケン'
+        expect(user.name_or_email).to eq 'ケン'
       end
     end
 
     context 'when name is not present' do
-      let!(:user_with_email) { create(:user, email: 'ken@example.com', name: '') }
+      let!(:user) { create(:user, email: 'ken@example.com', name: '') }
 
       it 'returns the email' do
-        expect(user_with_email.name_or_email).to eq 'ken@example.com'
+        expect(user.name_or_email).to eq 'ken@example.com'
       end
     end
   end
