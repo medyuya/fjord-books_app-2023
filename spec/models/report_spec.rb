2c28791e50e7746a46ed98b06aef98bd01223edf
yuyanishimura <yuya.nishimura@medpeer.co.jp>
2024-05-13 11:57:21 +0900

    Dataオブジェクトの生成に関して、可読的な書き方に直すため

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe Report do
     it 'returns the date when the report was created' do
       travel_to "2023-08-30 01:04:44".in_time_zone do
         report = FactoryBot.create(:report, user: user)
-        expect(report.created_on).to eq Date.new(2023, 8, 30)
+        expect(report.created_on).to eq "2023-08-30".to_date
       end
     end
   end
